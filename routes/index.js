11261521243600d6952db6973e5bb4a0353c19e7
Kirti2198 <kirti2198@gmail.com>
2020-03-06 21:38:16 +0530

    added one more route and controller for user-profile

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,8 @@ const homeController= require('../controllers/home_controller');
 console.log('router loaded');
 
 router.get('/', homeController.home);
+router.use('/users', require('./users'));
+// for any further routes
+// router.use('/routerName',require('./routerfile'));
 
 module.exports= router;
