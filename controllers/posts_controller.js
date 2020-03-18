d0a129ece4c434bbd8d22a51b774ec5a5b6b936d
Kirti2198 <kirti2198@gmail.com>
2020-03-18 20:13:59 +0530

    deleting a post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,7 @@
 const Post = require('../models/post')
-
+// accessing comments for deleting
+const Comment = require('../models/comment');
+// for creating a post(action)
 module.exports.create = function(req, res){
     Post.create({
         content: req.body.content,
@@ -7,5 +9,24 @@ module.exports.create = function(req, res){
     }, function(err, post){
         if(err){console.log('error in creating a post'); return;}
         return res.redirect('back');
+    });
+}
+// for deleting a post
+module.exports.destroy= function(req,res){
+    // first check if it exist
+    Post.findById(req.params.id, function(err,post){
+    // authorization if the user is the who make the post who is deleting
+    //  .id means converting the object id into string
+      if(post.user== req.user.id){
+             post.remove;
+             Comment.deleteMany({post: req.params.id}, function(err){
+                 return res.redirect('back');   
+             });
+      }
+      else
+      {
+          return res.redirect('back');
+      
+      }
     });
 }
