2d77c25033710187b0a95c216b9a45980768be81
Kirti2198 <kirti2198@gmail.com>
2020-03-19 17:44:17 +0530

    added flash messages

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-const express=require('express');
-const cookieParser=require('cookie-parser');
-const app=express();
-const port= 8001;
-// express-layout
-const expressLayouts= require('express-ejs-layouts');
-// require mongoose
-const db= require('./config/mongoose');
-// require express-session for using passport
-// use for session cookie
-const session=require('express-session');
-// require passport
-const passport=require('passport');
-// require passport-local(we need to require both passport and passport-local)
-const passportLocal= require('./config/passport-local-strategy');
-// require connect-mongo
-const MongoStore= require('connect-mongo')(session);
-// sass middleware
-const sassMiddleware= require('node-sass-middleware');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const app = express();
+const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo')(session);
+const sassMiddleware = require('node-sass-middleware');
+const flash = require('connect-flash');
+const customMware = require('./config/middleware');
+
 
 app.use(sassMiddleware({
     src: './assets/scss',
@@ -25,23 +21,25 @@ app.use(sassMiddleware({
     outputStyle: 'extended',
     prefix: '/css'
 }));
-// middleware
 app.use(express.urlencoded());
-// for cookie-parser
+
 app.use(cookieParser());
-// accessing the static files
+
 app.use(express.static('./assets'));
 
 app.use(expressLayouts);
-// extract style and scripts from sub pages into the layouts
-app.set('layout extractStyles',true);
-app.set('layout extractScripts',true);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
-// set up the view engine ejs
+
+
+
+// set up the view engine
 app.set('view engine', 'ejs');
-app.set('views','./views');
-// search it on google for more info
-// mongo store isused to store the session cookie in the db
+app.set('views', './views');
+
+// mongo store is used to store the session cookie in the db
 app.use(session({
     name: 'codeial',
     // TODO change the secret before deployment in production mode
@@ -49,36 +47,36 @@ app.use(session({
     saveUninitialized: false,
     resave: false,
     cookie: {
-        //  doscumentation-maxage.cookie- the milisecond 
-         maxAge: (1000 * 60 * 100) 
+        maxAge: (1000 * 60 * 100)
     },
-    store:new MongoStore(
+    store: new MongoStore(
         {
-        mongooseConnection: db,
-        autoRemove: 'disabled'
+            mongooseConnection: db,
+            autoRemove: 'disabled'
+        
         },
         function(err){
-        console.log(err || 'connect-mongodb setup ok');
+            console.log(err ||  'connect-mongodb setup ok');
         }
     )
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
+
 app.use(passport.setAuthenticatedUser);
 
-// use express router
-app.use('/', require('./routes'));
+app.use(flash());
+app.use(customMware.setFlash);
 
 // use express router
 app.use('/', require('./routes'));
+
 
 app.listen(port, function(err){
-    if(err){
-        // console.log('Error:',err);
-        // we can print error by another way interpolation as below
-        console.log(`Error in running the server: ${err}`);     
+    if (err){
+        console.log(`Error in running the server: ${err}`);
     }
-    console.log(`Server is running on Port: ${port}`);
 
+    console.log(`Server is running on port: ${port}`);
 });
