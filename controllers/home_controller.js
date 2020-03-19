7e713be9200a056dab7498554c2f1756800e8ea5
Kirti2198 <kirti2198@gmail.com>
2020-03-19 17:10:13 +0530

    converted code to async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,41 +1,42 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home= function(req,res){
-    // return res.end('<h1>Express is up for codeial!</h1>');
-    // printing the cookies on the terminal 
-    // console.log(req.cookies);
-    // change the value of cookie on response
-    // res.cookie('user_id',25); 
-
-// display the posts on the home page
-    // Post.find({}, function(err, posts){
-    //     return res.render('home', {
-    //         title: "Codeial | Home",
-    //         posts:  posts
-    //     });
-    // });
 
 
-    // populate the user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
+module.exports.home = async function(req, res){
 
-        User.find({}, function(err, users){
-            return res.render('home', {
-                title: "Codeial | Home",
-                posts:  posts,
-                all_users: users
-            });
+    try{
+         // populate the user of each post
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
         });
-    });   
-}
     
-// module.exports.actionName=function(req,res){return res.end}
+        let users = await User.find({});
+
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:  posts,
+            all_users: users
+        });
+
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
+   
+}
+
+// module.exports.actionName = function(req, res){}
+
+
+// using then
+// Post.find({}).populate('comments').then(function());
+
+// let posts = Post.find({}).populate('comments').exec();
+
+// posts.then()
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 
+// let's keep it same as before
 module.exports.profile = function(req, res){
     User.findById(req.params.id, function(err, user){
         return res.render('user_profile', {
@@ -10,7 +11,7 @@ module.exports.profile = function(req, res){
 
 }
 
-// for update
+
 module.exports.update = function(req, res){
     if(req.user.id == req.params.id){
         User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
@@ -21,26 +22,32 @@ module.exports.update = function(req, res){
     }
 }
 
-// render the signup page
-module.exports.signUp = function(req,res){
-    if(req.isAuthenticated()){
+
+// render the sign up page
+module.exports.signUp = function(req, res){
+    if (req.isAuthenticated()){
         return res.redirect('/users/profile');
     }
-    return res.render('user_sign_up',{
-        title:"Codeial | Sign Up"
-    });
-};
-// render the signin page
-module.exports.signIn = function(req,res){
-    if(req.isAuthenticated()){
-       return res.redirect('/users/profile');
-    }
-    return res.render('user_sign_in',{
-        title:"Codeial | Sign In"
-    });
-};
 
-// get the signup data
+
+    return res.render('user_sign_up', {
+        title: "Codeial | Sign Up"
+    })
+}
+
+
+// render the sign in page
+module.exports.signIn = function(req, res){
+
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+    return res.render('user_sign_in', {
+        title: "Codeial | Sign In"
+    })
+}
+
+// get the sign up data
 module.exports.create = function(req, res){
     if (req.body.password != req.body.confirm_password){
         return res.redirect('back');
@@ -54,20 +61,20 @@ module.exports.create = function(req, res){
                 if(err){console.log('error in creating user while signing up'); return}
 
                 return res.redirect('/users/sign-in');
-            });
+            })
         }else{
             return res.redirect('back');
         }
 
     });
-};
+}
+
 
 // sign in and create a session for the user
-
 module.exports.createSession = function(req, res){
     return res.redirect('/');
 }
-// for sign-out
+
 module.exports.destroySession = function(req, res){
     req.logout();
 
